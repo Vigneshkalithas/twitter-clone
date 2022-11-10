7eb34e951c39d57cf71c1ebe60ebdcaa965cb7c9
vigneshkalithas <vigneshkanna3333@gmail.com>
2022-11-10 14:50:30 +0530

    sidebar

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import logo from "./assets/icons8-twitter.svg";
-import home from "./assets/home.svg";
 import bell from "./assets/bell.svg";
 import account from "./assets/person.svg";
 import compose from "./assets/compose.svg";
 import mail from "./assets/mail.svg";
+import message from "./assets/message.svg";
 
 
 function SideBar() {
   return (
     <div className='sidebar-container'>
-     {/* <div><img className="logo" src={logo} alt="logo"/></div>
-     <div><img src={home} alt="home"/></div> */}
-     {/* <div><img src={bell} alt="bell"/></div>
-     <div><img src={mail} alt="mail"/></div>
-     <div><img src={account} alt="account"/></div>
-     <div><img src={compose} alt="compose"/></div> */}
+      <div className='icon-head'>
+      <div><img className="logo" src={logo} alt="logo"/></div>
+     <div><img src={bell} alt="bell" className='bell'/></div>
+     <div><img src={mail} alt="mail" className='mail'/></div>
+     <div><img src={account} alt="account" className='account'/></div>
+     <div><img src={message} alt="message" className='message'/></div>
+     <div><img src={compose} alt="compose" className='compose'/></div>
+
+      </div>
+     
 
 
     </div>
